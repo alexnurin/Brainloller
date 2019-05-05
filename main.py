71c899bebb5c672c7927192352526f1c140d9311
Нарин Алексей <an362@mail.ru>
2019-05-05 21:37:43 +0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import cv2 # нужно поставить себе cv2 через conda install opencv
 import numpy as np
+
+
 d = {
         (255, 0, 0): ">",  # red
         (128, 0, 0): "<",  # darkred
@@ -13,7 +15,6 @@ d = {
         (0, 255, 255): "R",  # cyan
         (0, 128, 128): "L"  # darkcyan
 }
-
 
 def draw_img(img, bgr=False):
     plt.figure(figsize=(10, 14))
@@ -29,16 +30,16 @@ def load_img(path):
 
 def save_img(name, image, exp = 'png'):
     name = '{}.{}'.format(name, exp)
+    if '.' in name:
+        name = '{}'.format(name)
     cv2.imwrite(name,image[:,:,::-1])
     print(name + " saved!")
 
 def change_img(old, img):
-    old = load_img(old)
     x = old.shape[0]
     y = old.shape[1]
     old = old.reshape(x*y, 3)
     img = img.reshape(img.shape[1], 3)
-    print(old.shape, img.shape)
     for i in range(min(img.shape[0], old.shape[0]//10)):
         old[i] = img[i]
     return old.reshape(x,y,3)
@@ -68,9 +69,29 @@ def code_to_img(code):
     return img
 
 
-def encode(image_name, bfcode):
-    img = change_image(image_name, bfcode)
-    save_img(image_name[:-4] + '_encoded', img)
+def encode(image_name, bfcode, new_name = 'empty', need_save = 1):
+    if new_name == 'empty':
+        new_name = image_name[:-4] + '_encoded'
+    old_img = load_img(image_name)
+    new_img = code_to_img(bfcode)
+    img = change_img(old_img, new_img)
+    if not need_save:
+        return img
+    save_img(new_name, img)
+    return "Encode successful"
 
 def decode(img):
-    return img_to_code(img = img.reshape(img.shape[0] * img.shape[1], 3))
+    img = load_img(img)
+    x = img.shape[0]
+    y = img.shape[1]
+    return img_to_code(img = img.reshape(x * y, 3))
+
+
+def main():
+    print("Example:")
+    code = '-[------->+<]>+++.+[--->+<]>.+++.-------------.++++++++.[->+++++<]>-.-[--->++<]>-.+++++++.+++.++.--------.'
+    encode("jojo.png", code)
+    print(decode("jojo_encoded.png"))
+
+
+main()
