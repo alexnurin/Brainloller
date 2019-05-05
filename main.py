89cd8a15b599de4f136402161f55ea106143a3fe
Нарин Алексей <an362@mail.ru>
2019-05-05 21:08:36 +0300

    Add requirements, readmi. Add dict in main

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import matplotlib.pyplot as plt
-import cv2
+import cv2 #conda install opencv
+import numpy as np
+
+
+d = {
+        (255, 0, 0): ">",  # red
+        (128, 0, 0): "<",  # darkred
+        (0, 255, 0): "+",  # green
+        (0, 128, 0): "-",  # darkgreen
+        (0, 0, 255): ".",  # blue
+        (0, 0, 128): ",",  # darkblue
+        (255, 255, 0): "[",  # yellow
+        (128, 128, 0): "]",  # darkyellow
+        (0, 255, 255): "R",  # cyan
+        (0, 128, 128): "L"  # darkcyan
+}
+
 
 def draw_img(img, bgr=False):
     plt.figure(figsize=(10, 14))
@@ -9,7 +25,7 @@ def draw_img(img, bgr=False):
 
 def load_img(path):
     img = cv2.imread(path)
-    b, g, r = cv2.split(img) # по умолчанию cv2 почему-то отдает цвета в порядке BGR вместо RGB
+    b, g, r = cv2.split(img) 
     new_image = cv2.merge([r, g, b])
     return new_image
 
@@ -17,3 +33,4 @@ def save_img(name, image, exp = 'png'):
     name = '{}.{}'.format(name, exp)
     cv2.imwrite(name,image[:,:,::-1])
     print(name + " saved!")
+
